495193034eaf086bc35b4ae1cdde14d832285794
Vrushabh Suchak <37805346+vrushabh1010@users.noreply.github.com>
2019-01-30 00:21:00 +0530

    Used Logcat in Android Studio

--- app/src/main/java/com/example/vrushabh/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/vrushabh/notekeeper/NoteActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -13,6 +14,7 @@ import android.widget.Spinner;
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
     public static final String NOTE_POSITION = "com.example.vrushabh.notekeeper.NOTE_POSITION";
     public static final String ORIGINAL_NOTE_COURSE_ID = "com.example.vrushabh.notekeeper.ORIGINAL_NOTE_COURSE_ID";
     public static final String ORIGINAL_NOTE_TITLE = "com.example.vrushabh.notekeeper.ORIGINAL_NOTE_TITLE";
@@ -55,6 +57,8 @@ public class NoteActivity extends AppCompatActivity {
 
         if(!mIsNewNote)
             displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
+
+        Log.d(TAG, "onCreate");
     }
 
     private void restoreOriginalNoteValues(Bundle savedInstanceState) {
@@ -76,6 +80,7 @@ public class NoteActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         if(mIsCancelling){
+            Log.i(TAG, "Cancelling note at position " + mNotePosition);
             if(mIsNewNote){
                 DataManager.getInstance().removeNote(mNotePosition);
             }else{
@@ -84,6 +89,8 @@ public class NoteActivity extends AppCompatActivity {
         }else {
             saveNote();
         }
+
+        Log.d(TAG, "onPause");
     }
 
     private void storePreviousNoteValues() {
@@ -122,7 +129,7 @@ public class NoteActivity extends AppCompatActivity {
         if(mIsNewNote){
             createNewNote();
         }
-
+            Log.i(TAG, "mNOtePosition: " + mNotePosition);
             mNote = DataManager.getInstance().getNotes().get(mNotePosition);
 
     }
