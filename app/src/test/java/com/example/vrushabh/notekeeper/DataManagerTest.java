2b762c17901ae80d3521f4217f76cff88749d47a
Vrushabh Suchak <37805346+vrushabh1010@users.noreply.github.com>
2019-02-24 13:56:41 +0530

    Implementing and running a unit test.

---
+++ app/src/test/java/com/example/vrushabh/notekeeper/DataManagerTest.java
@@ -0,0 +1,28 @@
+package com.example.vrushabh.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    @Test
+    public void createNewNote() throws Exception{
+        final DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body text of my test note";
+
+        int noteIndex = dm.createNewNote();
+        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+
+        assertEquals(compareNote.getCourse(), course);
+        assertEquals(compareNote.getTitle(), noteTitle);
+        assertEquals(compareNote.getText(), noteText);
+    }
+}
