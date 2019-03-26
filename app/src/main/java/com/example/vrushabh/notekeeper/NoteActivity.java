403889a8c1f74df4ca7ddb1e25e22a30798177d2
Vrushabh Suchak <37805346+vrushabh1010@users.noreply.github.com>
2019-03-26 17:11:26 +0530

    Used options menu in NoteActivity.

--- app/src/main/java/com/example/vrushabh/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/vrushabh/notekeeper/NoteActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -161,9 +162,29 @@ public class NoteActivity extends AppCompatActivity {
         } else if(id == R.id.action_cancel){
             mIsCancelling = true;
             finish();
+        }else if(id == R.id.action_next){
+            moveNext();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.action_next);
+        int lastNoteIndex = DataManager.getInstance().getNotes().size() - 1;
+        item.setEnabled(mNotePosition < lastNoteIndex);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    private void moveNext() {
+        saveNote();
+
+        ++mNotePosition;
+        mNote = DataManager.getInstance().getNotes().get(mNotePosition);
+        saveOriginalNoteValues();
+        displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
+        invalidateOptionsMenu();
     }
 
     private void sendEmail() {
