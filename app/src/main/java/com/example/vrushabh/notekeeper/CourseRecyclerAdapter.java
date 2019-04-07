a009eea0d7d605287a4ac1307d962e6e6d213c09
Vrushabh Suchak <37805346+vrushabh1010@users.noreply.github.com>
2019-04-07 17:50:20 +0530

    Adding NavigationDrawer and handling NavigationView options

---
+++ app/src/main/java/com/example/vrushabh/notekeeper/CourseRecyclerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.vrushabh.notekeeper;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CourseRecyclerAdapter extends RecyclerView.Adapter<CourseRecyclerAdapter.ViewHolder> {
+    private final Context mContext;
+    private final List<CourseInfo> mCourses;
+    private final LayoutInflater mLayoutInflater;
+
+
+    public CourseRecyclerAdapter(Context mContext, List<CourseInfo> mCourses) {
+        this.mContext = mContext;
+        mLayoutInflater = LayoutInflater.from(mContext);
+        this.mCourses = mCourses;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_course_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CourseInfo course = mCourses.get(position);
+        holder.mTextCourse.setText(course.getTitle());
+        holder.mCurrentPosition = position;
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourses.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView mTextCourse;
+        public int mCurrentPosition;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Snackbar.make(v, mCourses.get(mCurrentPosition).getTitle(),
+                            Snackbar.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
