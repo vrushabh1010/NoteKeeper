288012b2b1d601f51591bc22571a7c892df89815
Vrushabh Suchak <37805346+vrushabh1010@users.noreply.github.com>
2019-04-09 17:07:12 +0530

    Using Drawables and Values Resources

--- app/src/main/java/com/example/vrushabh/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/vrushabh/notekeeper/MainActivity.java
@@ -66,7 +66,8 @@ public class MainActivity extends AppCompatActivity
 
         mRecyclerItems = (RecyclerView) findViewById(R.id.list_items);
         mNotesLayoutManager = new LinearLayoutManager(this);
-        mCoursesLayoutManager = new GridLayoutManager(this,2);
+        mCoursesLayoutManager = new GridLayoutManager(this,
+                getResources().getInteger(R.integer.course_grid_span));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
@@ -140,9 +141,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_courses) {
             displayCourses();
         }  else if (id == R.id.nav_share) {
-            handleSelection("Don't you think you've shared enough?");
+            handleSelection(R.string.nav_share_message);
         } else if (id == R.id.nav_send) {
-            handleSelection("Send");
+            handleSelection(R.string.nav_send_message);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -150,8 +151,8 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void handleSelection(String message) {
+    private void handleSelection(int message_id) {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view, message_id, Snackbar.LENGTH_LONG).show();
     }
 }
